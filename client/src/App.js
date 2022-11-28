113c0f8bfbaa6e628d709a58b7db53981a9d113a
Charlee Gordon <cgordon5025@gmail.com>
2022-11-28 18:31:06 -0500

    navbar framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+//importing from npm packages
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
+import { Routes, Route } from 'react-router-dom'
+import { setContext } from '@apollo/client/link/context'
+//pages
 
+//components
+import Navbar from './components/Navbar'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ``,
+    },
+  };
+})
+
+const httpLink = createHttpLink({
+  uri: 'graphql',
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloClient client={client}>
+      <Navbar />
+      <Routes>
+        {/* <Route path='/' index element={<Home />} /> */}
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloClient>
   );
 }
 
---
+++ client/src/components/Navbar/index.js.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Auth from '../../utils/auth'
+
+const Navbar = () => {
+    return (
+        <>
+            <header>
+                <nav className='navbar navbar-expand-lg'>
+                    <Link className="navbar-brand" to="/">Home Page</Link>
+                    <div id='navbarNav' style={{ marginLeft: "auto" }}>
+                        <ul className='nav-item'>
+                            {/* space for other links */}
+                            {Auth.loggedIn() ?
+                                (<li className="nav-item ">
+                                    <Link onClick={Auth.logout} className="nav-link" to="/">Logout</Link>
+                                </li>) :
+                                (<li className="nav-item ">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>)}
+
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
