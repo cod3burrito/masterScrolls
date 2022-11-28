7a97ffe7c25582c8071ad2a097a2cbf175493318
Joseph A <joseph_anderson11@yahoo.com>
2022-11-28 16:59:57 -0500

    auth.js set up to implement JWT

---
+++ server/utils/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const secret = "keepitsecret"
+const expiration = "24h"
+
+module.exports = {
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        try{
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        }catch {
+            console.log('Invalid token');
+        }
+
+        return req;
+    },
+    signToken: function ({ username, email, _id, campaigns }){
+        const payload = { username, email, _id, campaigns };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiriation });
+    }
+}
