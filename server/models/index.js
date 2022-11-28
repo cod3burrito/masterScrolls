d194a316ac87aef6cd9be31cff8ec32a00b40eae
Charlee Gordon <cgordon5025@gmail.com>
2022-11-28 10:42:47 -0500

    character model done

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Character = require('./Character')
+const Campaign = require('./Campaign')
+const Location = require('./Location')
+const User = require('./User')
+
+module.exports = {User, Campaign, Location, Character}
