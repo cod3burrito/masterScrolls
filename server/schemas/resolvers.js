66efe2dc0b97d8b673b7ea78710640717302adba
Charlee Gordon <cgordon5025@gmail.com>
2022-11-28 11:12:34 -0500

    framework for resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,49 @@
-const {User, Campaign} = require('../models');
+const { User, Campaign } = require('../models');
 
 const resolvers = {
+    Query: {
+        getUser: async () => {
+
+        },
+        getCampaign: async () => {
+
+        },
+    },
+    Mutations: {
+        createUser: async () => {
+
+        },
+        deleteUser: async () => {
+
+        },
+        createCampaign: async () => {
+
+        },
+        editCampaign: async () => {
+
+        },
+        deleteCampaign: async () => {
+
+        },
+        createLocation: async () => {
+
+        },
+        editLocation: async () => {
+
+        },
+        deleteLocation: async () => {
+
+        },
+        createCharacter: async () => {
+
+        },
+        editCharacter: async () => {
+
+        },
+        deleteCharacter: async () => {
+
+        }
+    },
 
 };
 
