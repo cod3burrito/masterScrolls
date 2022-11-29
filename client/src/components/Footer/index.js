2d795b0424f107ac47d14e6e147be81fc287002d
cod3burrito <robintanner44@gmail.com>
2022-11-29 11:26:18 -0500

    made myself a little joke

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Smash that like button, ring that bell, and subscribe to our Patreon.<br />
+                &copy; 2022 JA, RA, CG, and CT
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
