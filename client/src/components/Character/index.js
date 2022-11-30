637788389af429695745243ee8ef062a879730d8
Charlee Gordon <cgordon5025@gmail.com>
2022-11-29 20:46:19 -0500

    single display

--- client/src/components/Character/index.js
+++ client/src/components/Character/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
-const Character = () => {
+const Character = ({ character }) => {
     return (
-        <h2> hello</h2>
+        <div className='card'>
+            <h2 className='card-header'> {character.name}</h2>
+            <div className='card-body'>
+                <p>{character.class}</p>
+                <p>{character.level}</p>
+                <p>{character.alive}</p>
+                <p>{character.goals}</p>
+                <p> Allies:</p>
+                <ul>
+                    {character.allies.map((ally) => {
+                        return (
+                            <li>{ally}</li>
+                        )
+                    })}
+                </ul>
+                <p> Notes:</p>
+                <ul>
+                    {character.notes.map((note) => {
+                        return (
+                            <li>{note}</li>
+                        )
+                    })}
+                </ul>
+                <p></p>
+            </div>
+
+        </div>
     )
 }
 
