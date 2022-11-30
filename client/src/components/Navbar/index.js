4e96f476be8b93bf5d1328c7971a176a0e24f0b2
Joseph A <joseph_anderson11@yahoo.com>
2022-11-30 18:35:47 -0500

    display random character on all pages

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Auth from '../../utils/auth'
+import { NPCs } from "fantasy-content-generator";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 const Navbar = () => {
+    const [randomCharacter, setRandomCharacter] = useState({name: "", gender: "", race: "", desires: [], traits: []})
+    const [show, setShow] = useState(false);
+
+
+    const log = () => {
+        const {name, gender, race, desires, traits} = {...NPCs.generate().formattedData}
+        setRandomCharacter({
+            name,
+            gender,
+            race,
+            desires,
+            traits
+        });
+
+        handleShow();
+        
+        
+    }
+
+    const handleClose = () => {
+        setShow(false);
+        setRandomCharacter({
+            name: "",
+            gender: "",
+            race: "",
+            desires: [],
+            traits: []
+        });
+    }
+
+    const handleShow = () => setShow(true);
+
     return (
         <>
             <header>
@@ -10,6 +46,9 @@ const Navbar = () => {
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <Link className="navbar-brand" to="/">Home Page</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link onClick={log} className="navbar-brand">Generate Random Character</Link>
                             </li>
                             
                             {Auth.loggedIn() ?
@@ -30,6 +69,27 @@ const Navbar = () => {
                     </div>
                 </nav>
             </header>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Random Character</Modal.Title>                   
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Name: {randomCharacter.name}</p>
+                    <p>Race: {randomCharacter.race}</p>
+                    <p>Gender: {randomCharacter.gender}</p>
+                    <p>Desires:</p>
+                    <ul>
+                        {randomCharacter.desires.map(desire => <li key={desire}>{desire}</li>)}
+                    </ul>
+                    <p>Traits</p>
+                    <ul>
+                        {randomCharacter.traits.map(trait => <li key={trait}>{trait}</li>)}
+                    </ul>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
