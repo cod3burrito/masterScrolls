220eb362f5317a918f250bfe86d8e0a0582a52dd
Joseph A <joseph_anderson11@yahoo.com>
2022-12-01 10:53:11 -0500

    wildcard set into conditional

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+      });
 }
-
-app.get('/*', (req, res) => res.send('Index Page'));
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
