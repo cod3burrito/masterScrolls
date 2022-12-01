fde8a2a2829f1cdf5d1494bc74a536eed8ef8566
Joseph A <joseph_anderson11@yahoo.com>
2022-12-01 10:59:54 -0500

    new res.send

--- server/server.js
+++ server/server.js
@@ -20,7 +20,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get("/*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+        // res.sendFile(path.join(__dirname, "./client/build/index.html"));
+        res.send("index.html")
       });
 }
 
