ac5bae68e97e35fa331fb059fd28148c76413f01
Joseph A <joseph_anderson11@yahoo.com>
2022-12-01 11:00:42 -0500

    change

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get("/*", function(req, res) {
         // res.sendFile(path.join(__dirname, "./client/build/index.html"));
-        res.send("index.html")
+        res.send("./index.html")
       });
 }
 
