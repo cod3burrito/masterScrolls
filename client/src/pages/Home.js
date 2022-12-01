0d834d4aa06f85776e34f5491da9601dbbbef1f6
cod3burrito <robintanner44@gmail.com>
2022-12-01 10:47:56 -0500

    pulling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import videoBG from '../assets/paper.mp4'
 
-const Home = () => { return (
+const videoBG = () => {return (
+    <video autoPlay id='pikapaper'>
+        <source src='{videoBG}' type='video/mp4' />
+    </video>
+)}
+
+function Home() {
+    return (
         <div className='welcome'><h1> Welcome, mortal! </h1>
-        <p> Please identify yourself in our ranks or request to join us. See the appropriate links above. </p></div>
+            <p> Please identify yourself in our ranks or request to join us.See the appropriate links above.</p></div>
     )
 }
 
